3020f932819be19f2b1a85250b054c22b027f837
WORKSPACE <yurii.oksamytnyi@yuriodev.co.uk>
2025-04-21 13:25:21 +0100

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,16 +1,19 @@
-# utils/__init__.py
-# Copyright (c) 2025 Perceptron Party Team (Yurii, Pyry, Dimitris, Dimitar)
+# Two Tower Search
+# File: utils/__init__.py
+# Description: Package initialization for utils module.
 # Created: 2025-04-21
+# Updated: 2025-04-21
 
+# Import the configured logger instance from logging.py
+# The setup_logging() function in logging.py runs automatically on this import.
 from .logging import logger
+
+# Import the get_device function from device_setup.py
 from .device_setup import get_device
-from .run_utils import (
-    format_num_words, load_config, save_losses, plot_losses
-)
 
-__all__ = [
-    'logger', 'get_device', 'format_num_words', 'load_config',
-    'save_losses', 'plot_losses'
-]
+# Define what gets imported with 'from utils import *'
+__all__ = ['logger', 'get_device']
 
-logger.debug("Utils package initialized.")
+# Optional: Log that the package is being initialized
+# Note: logger might already be configured here due to import above
+logger.debug("Utils package initialized (__init__.py).")
